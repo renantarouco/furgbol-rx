863c9afdcf72623c0839f25d97adfdbef5a10a17
Renan Tarouco da Fonseca <renantarouco@gamil.com>
2019-07-14 18:23:28 -0300

    Still trying the dynamic operator form protobuf::Message*.

--- src/operators/parse_referee.cc
+++ src/operators/parse_referee.cc
@@ -2,12 +2,12 @@
 
 namespace furgbol {
 namespace operators {
-    std::function<rxcpp::observable<SSL_Referee*>(rxcpp::observable<std::string>)> parse_referee() {
+    std::function<rxcpp::observable<SSL_Referee>(rxcpp::observable<std::string>)> parse_referee() {
         return [](rxcpp::observable<std::string> datagram) {
             return datagram |
                 rxcpp::operators::map([](std::string data){
-                    SSL_Referee *referee = new SSL_Referee;
-                    if (!referee->ParseFromString(data)) {
+                    SSL_Referee referee;
+                    if (!referee.ParseFromString(data)) {
                         throw std::runtime_error("wrapper packet parsing error");
                     }
                     return referee; });};
--- src/operators/serialize_to_json.cc
+++ src/operators/serialize_to_json.cc
@@ -4,15 +4,15 @@
 
 namespace furgbol {
 namespace operators {
-    std::function<rxcpp::observable<std::string>(rxcpp::observable<google::protobuf::Message*>)> serialize_to_json() {
-        return [](rxcpp::observable<google::protobuf::Message*> message) {
+    std::function<rxcpp::observable<std::string>(rxcpp::observable<google::protobuf::Message&>)> serialize_to_json() {
+        return [](rxcpp::observable<google::protobuf::Message&> message) {
             return message |
                 rxcpp::operators::map(
-                    [](google::protobuf::Message *message) {
+                    [](google::protobuf::Message &message_) {
                         google::protobuf::util::JsonPrintOptions options;
                         options.always_print_primitive_fields = true;
                         std::string json_str;
-                        google::protobuf::util::MessageToJsonString(*message, &json_str, options);
+                        google::protobuf::util::MessageToJsonString(message_, &json_str, options);
                         return json_str; });};
     }
 }
